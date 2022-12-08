ca00abffe91f9ed47c1542c302367b20fde89091
Roman <popov.roman.e@gmail.com>
2022-12-09 01:25:25 +0500

    добавляет компоненты

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import NewCreationFormView from './view/creation-form.js';
+import NewEditFormView from './view/edit-form.js';
+import NewFilterElementView from './view/filter-element.js';
+import NewFilterFormView from './view/filter-form.js';
+import NewSortFormView from './view/sort-form.js';
+import NewSortElementView from './view/sort-element.js';
+
+
+import render from './render.js';
+
+
---
+++ src/view/sort-form.js
@@ -0,0 +1,24 @@
+import {createElement} from '../render.js';
+
+function createNewSortFormTemplate() {
+  return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+</form>`;
+}
+
+export default class NewSortFormView {
+  getTemplate() {
+    return createNewSortFormTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
